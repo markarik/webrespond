ead3eacdf8c120e8e38682294dd9da1fe63f8ddd
markarik <kariukimarkg@gmail.com>
2019-01-21 11:36:55 +0300

    second commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+
+
+function eventlisteners(){
+    document.querySelector('.navbtn').addEventListener('click' , function(){
+        document.querySelector('.nav').classList.toggle('nav--show')
+    })
+
+    /* video switch */
+    
+}
+function btnswitchs(){
+    document.querySelector('.videoswitch').addEventListener('click' , function(){
+
+
+        let btn = document.querySelector('.videoswitch_btn');
+      
+        if(!btn.classList.contains('btnslide')) {
+            btn.classList.add('btnslide')
+            document.querySelector('.videoitem').pause();
+           
+        } 
+        else{
+            btn.classList.remove('btnslide')
+            document.querySelector('.videoitem').play();
+        }  
+        
+    })
+}
+
+    eventlisteners(); 
+    btnswitchs();
+    
+
+
+    /* let button = document.querySelector(.videoswitch_btn');
+  button.addEventListener("click", () => {
+    console.log("Button clicked."); */
